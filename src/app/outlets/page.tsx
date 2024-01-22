feeba23c42a8ca2d24f204928b5c2465b7f33923
Viraj Mathur <virajmathur2002@gmail.com>
2024-01-22 23:38:34 +0530

    Fixed 'getOutlets()' problem

--- src/app/outlets/page.tsx
+++ src/app/outlets/page.tsx
@@ -2,15 +2,7 @@ import Link from "next/link";
 import OutletCard from "@/components/outletCard/outletCard";
 import styles from "@/app/outlets/page.module.css";
 import mongoClientPromise from "@/lib/mongodb/dbConnect";
-
-export const getOutlets = async () => {
-  const dbConnection = await mongoClientPromise;
-  const collection = dbConnection.db("CampusBites").collection("outlets");
-
-  let result = await collection.find({}).toArray();
-
-  return result;
-};
+import { getOutlets } from "@/actions/getActions";
 
 export default async function Outlets() {
   const outlets = await getOutlets();
